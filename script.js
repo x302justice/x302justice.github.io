09163a7febac4de32ae3ead4ca1c38b024727391
x302justice <56979069+x302justice@users.noreply.github.com>
2020-11-15 16:14:29 +0000

    Summary (required)

--- script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 20 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "20";
-  } 
