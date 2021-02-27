b68982228d3e0693c93f04167949259fd13a9030
x302justice <56979069+x302justice@users.noreply.github.com>
2021-02-27 11:25:49 +0000

    Summary (required)

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function myFunction() {
     var str = "get decieved >:D";
     var result = str.fontcolor("yellow");
     document.getElementById("demo").innerHTML = result;
-  }
+    } 
+function Alert() {
+    alert("A S S")
+    return 0;
+    };
